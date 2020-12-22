3c2b25f7a2d1bbe3304a2fc2f12b803a5a7f33ca
PorterOBrien2021 <824085@mystma.org>
2020-12-22 16:14:02 +0000

    test

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-def name():
-    return "n"
-
+def name(n):
+    return (n)
+print(name("Porter O'Brien"))
 def favorites(favNum, favCol):
-    return favNum + " " + favCol
+    return "my favorite number is 64" + " " + "my favorite color is blue"
+print (favorites(favNum, favCol))
 
 def bigAdd(x, y, z, a, b):
     return x + y + z + a + b
 
 def fourOperations(x, y, z, a, b):
-    return x + y - z * a - b
+    return x + y - z * a / b
 
 print(name())

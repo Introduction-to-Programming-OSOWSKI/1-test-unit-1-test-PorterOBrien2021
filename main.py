fa660ff72fd2a708204eb51eda52d843be912a4f
PorterOBrien2021 <824085@mystma.org>
2020-12-22 16:01:38 +0000

    test

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def name():
     return "n"
 
-def favorites():
-    return "favNum, favCol"
+def favorites(favNum, favCol):
+    return favNum + " " + favCol
 
 def bigAdd(x, y, z, a, b):
     return x + y + z + a + b
